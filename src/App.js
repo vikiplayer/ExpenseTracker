1bb1542e6f69ca9fc696333ef31a8fdfd0a0b084
vikiplayer <mvigneshwaran79@gmail>
2024-09-19 17:59:52 +0530

    fiest commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/UI/Card";
+import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Card>
+      <Card>
+        <NewExpense/>  
+      </Card>
+      <Card>
+        <Expenses/>  
+      </Card>
+    </Card>
+  )
 }
 
 export default App;
